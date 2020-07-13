b666c6da04b25576c2c07737e34b0451e56335d4
LALBJ <905976755@qq.com>
2020-07-13 22:53:06 +0800

    之前上传，部分功能存在问题，修复了问题

--- src/costomerFeature.vue
+++ src/costomerFeature.vue
@@ -84,118 +84,65 @@ export default {
     computed: {
     },
     methods: {
-      drawStoreType() {
-          var pieWidth = getComputedStyle(document.getElementsByClassName("storeLoc")[0],false).width;
-          var pieHeight = "300px";
-        d3.select("#storeType")
-            .style("width", pieWidth)
-            .style("height", pieHeight);
-        
-        //var echarts = require('echarts');
-        var option = {
+      drawAgeBar() {
+          var option = {
+            color: ['#3398DB'],
             tooltip: {
-                trigger: 'item',
-                formatter: '{a} <br/>{b}: {c} ({d}%)'
+                trigger: 'axis',
+                axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
             },
-            legend: {
-                orient: 'vertical',
-                left: 10,
-                data: ['休闲', '服务', '购物', '餐饮', '娱乐', '丽人', '旅游', '运动', '金融', '酒店', '生活', '汽车', '大型商场', '超市', '便利店', '集市', '家电数码', '家具建材', '中餐厅', '轻餐厅', '快餐厅', '外国餐厅']
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
             },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: ['19岁以下', '19-25岁', '26-30岁', '31-35岁', '36-40岁', '40岁以上'],
+                    axisTick: {
+                        alignWithLabel: true
+                    }
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value'
+                }
+            ],
             series: [
                 {
-                    name: '商铺详情',
-                    type: 'pie',
-                    selectedMode: 'single',
-                    radius: [0, '30%'],
-
-                    label: {
-                        position: 'inner'
-                    },
-                    labelLine: {
-                        show: false
-                    },
-                    data: [
-                        {value: 1984, name: '休闲'},
-                        {value: 2941, name: '服务'},
-                        {value: 1209, name: '购物'},
-                        {value: 6709, name: '餐饮'}
-                    ]
-                },
-                {
-                    name: '商铺详情',
-                    type: 'pie',
-                    radius: ['40%', '55%'],
-                    label: {
-                        formatter: '{a|{a}}{abg|}\n{hr|}\n  {b|{b}：}{c}  {per|{d}%}  ',
-                        backgroundColor: '#eee',
-                        borderColor: '#aaa',
-                        borderWidth: 1,
-                        borderRadius: 4,
-                        // shadowBlur:3,
-                        // shadowOffsetX: 2,
-                        // shadowOffsetY: 2,
-                        // shadowColor: '#999',
-                        // padding: [0, 7],
-                        rich: {
-                            a: {
-                                color: '#999',
-                                lineHeight: 22,
-                                align: 'center'
-                            },
-                            // abg: {
-                            //     backgroundColor: '#333',
-                            //     width: '100%',
-                            //     align: 'right',
-                            //     height: 22,
-                            //     borderRadius: [4, 4, 0, 0]
-                            // },
-                            hr: {
-                                borderColor: '#aaa',
-                                width: '100%',
-                                borderWidth: 0.5,
-                                height: 0
-                            },
-                            b: {
-                                fontSize: 16,
-                                lineHeight: 33
-                            },
-                            per: {
-                                color: '#eee',
-                                backgroundColor: '#334455',
-                                padding: [2, 4],
-                                borderRadius: 2
-                            }
-                        }
-                    },
-                    data: [
-                        {value: 450, name: '娱乐'},
-                        {value: 970, name: '丽人'},
-                        {value: 327, name: '旅游'},
-                        {value: 237, name: '运动'},
-                        {value: 811, name: '金融'},
-                        {value: 552, name: '酒店'},
-                        {value: 1515, name: '生活'},
-                        {value: 63, name: '汽车'},
-                        {value: 81, name: '大型商场'},
-                        {value: 115, name: '超市'},
-                        {value: 306, name: '便利店'},
-                        {value: 99, name: '集市'},
-                        {value: 198, name: '家电数码'},
-                        {value: 491, name: '家具建材'},
-                        {value: 1492, name: '中餐厅'},
-                        {value: 1719, name: '轻餐厅'},
-                        {value: 2430, name: '快餐厅'},
-                        {value: 895, name: '外国餐厅'}
-                    ]
+                    name: '直接访问',
+                    type: 'bar',
+                    barWidth: '60%',
+                    data: [1.3, 23.1, 29.3, 26.4, 11.5, 8.4]
                 }
             ]
         };
-        // 基于准备好的dom，初始化echarts实例
-        console.log(document.getElementById('storeType'))
-        var myChart = echarts.init(document.getElementById('storeType'), 'dark');
+        var myChart = echarts.init(document.getElementById('ageBar'), 'light');
         myChart.setOption(option);
-    }
+      },
+      drawLine() {
+          var option = {
+            xAxis: {
+                type: 'category',
+                data: ['0-3000元', '3000-5000元', '5000-8000元', '8000-15000元', '15000元以上']
+            },
+            yAxis: {
+                type: 'value'
+            },
+            series: [{
+                data: [12.1, 14.9, 23.4, 28.5, 20.3],
+                type: 'line'
+            }]
+        };
+        var myChart = echarts.init(document.getElementById('wageLine'), 'light');
+        myChart.setOption(option);
+
+      }
     },
     props:{
         ids:{
